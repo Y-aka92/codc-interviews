0a52fab3bd11d9c8b6f931a49a4d9f7bdb9e8899
PLANON-FM\yaakag <yalcin.akagunduz@planonsoftware.com>
2024-02-22 11:39:44 +0100

    new test code

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,20 +1,33 @@
 import unittest
 import sys
+from pyspark.sql import SparkSession
+from pyspark.sql import Row
 sys.path.append('../src')
-from unittest.mock import Mock
 from main import filter_clients_by_country
 
 class TestMainMethods(unittest.TestCase):
-    
+    # setUp method to initialize SparkSession and create a test DataFrame
     def setUp(self):
-        self.mock_spark = Mock()
-        self.mock_df = Mock()
-    
+        self.spark = SparkSession.builder \
+            .appName("TestApp") \
+            .getOrCreate()
+        # Create test data
+        self.test_data = [Row(country="Netherlands"), Row(country="United Kingdom"), Row(country="Germany")]
+        self.df = self.spark.createDataFrame(self.test_data)
+
+    # Method to test the filter_clients_by_country function
     def test_filter_clients_by_country(self):
-        self.mock_df.filter.return_value = 'gefilterde DataFrame'
-        resultaat = filter_clients_by_country(self.mock_df, ["Netherlands", "United Kingdom"])
-        self.mock_df.filter.assert_called_with("country IN ('Netherlands', 'United Kingdom')")
-        self.assertEqual(resultaat, 'gefilterde DataFrame')
+        # Call the function with the test DataFrame
+        resultaat = filter_clients_by_country(self.df, ["Netherlands", "United Kingdom"])
+        # Expected output
+        expected_data = [Row(country="Netherlands"), Row(country="United Kingdom")]
+        expected_df = self.spark.createDataFrame(expected_data)
+        # Assert if the result is as expected
+        self.assertEqual(resultaat.collect(), expected_df.collect())
+
+    # tearDown method to stop the SparkSession
+    def tearDown(self):
+        self.spark.stop()
 
 if __name__ == '__main__':
     unittest.main()
